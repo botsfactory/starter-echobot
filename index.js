5d9cec15b754064df4b8ad124e6c10536d928c38
Pablo Costa <pablo@thebotmakers.com>
2017-09-15 18:47:28 -0300

    First version using @botsfactory/botsfactory

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 //@ts-check
-require('source-map-support').install();
-
-const path = require('path')
+const path = require('path');
+const mongoose = require('mongoose');
 const botbuilder = require('botbuilder');
 const _ = require("lodash");
 const moment = require("moment");
 const express = require("express");
-const router = express.Router()
-const querystring = require('querystring');
+const router = express.Router();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-// const cmusers = require("@botsfactory/modules/src/users");
-// const cmconversation = require("@botsfactory/modules/src/conversation");
+const botsfactory = require('@botsfactory/botsfactory');
 
 const connector = new botbuilder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
@@ -21,57 +18,33 @@ const connector = new botbuilder.ChatConnector({
 const server = express();
 var bot = new botbuilder.UniversalBot(connector);
 
-bot.dialog('/', function(session){
+bot.dialog('/', function (session) {
     session.endDialog("I heard: %s", session.message.text);
-})
+});
 
+mongoose.connect(process.env.DB_URI).then(() => {
 
-server.use(function (req, res, next) {
+    //LET'S DO IT!
+    //botsfactory.install(bot, server, mongoose.connection.db, process.env.DB_URI)
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Handle Bot Framework messages
+    server.post('/api/messages', connector.listen());
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    const listener = server.listen(process.env.PORT || 8989, function () {
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-    // Pass to next layer of middleware
-    next();
+        console.log('Bot started listening on', listener.address().address, listener.address().port);
+    });
 });
 
 
-server.use(bodyParser.json())
-server.use(methodOverride())
-
-server.use(router)
 
 
-// this is for localtunnel
-server.get('/', (req, res) => res.send('hola'))
-
-// Handle Bot Framework messages
-server.post('/api/messages', connector.listen());
 
 
-// cmusers.install(bot, db, server, { FACEBOOK_PAGE_TOKEN: process.env.FACEBOOK_PAGE_TOKEN })
-
-// //cmanalytics.install(bot, process.env.DASHBOT_API_KEY);
-
-// //
-// cmconversation.installGeneric(bot, db);
-
-//
-//cmerrors.installRollbarReporter(bot, { token: process.env.ROLLBAR_TOKEN, environment: process.env.NODE_ENV });
 
 
-const listener = server.listen(process.env.PORT || 8989, function () {
 
-    console.log('Bot started listening on', listener.address().address, listener.address().port);
-})
+
+
+
 
